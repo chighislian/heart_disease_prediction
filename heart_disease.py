e9ef79ae8860c22968cdf7de086c95070311be3c
Ghislian Chigozie <chigozieghislian@gmail.com>
2025-11-05 04:07:54 +0100

    did check for missing values

---
+++ heart_disease.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+df = pd.read_csv(r"heart_disease_data.csv")
+print(df.head())
+print(df.info())
+
+
